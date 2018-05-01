be8bd43b0aea7984b24c05da8ab6a1fa9d8f24a7
AriadniAdi <ariadniadi@gmail.com>
2018-04-30 23:48:12 -0300

    Adicionando celulas

--- App.js
+++ App.js
@@ -10,7 +10,8 @@ import {
   StyleSheet,
   Text,
   View,
-  FlatList
+  FlatList,
+  CheckBox
 } from 'react-native';
 
 const instructions = Platform.select({
@@ -22,33 +23,62 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+  getTasks() {
+    let items = []
+    for(var i = 0; i < 20 ; i++) {
+      items.push({ id: '1', title: 'título', date: '17/11/2018', hour: '16:00', done: false })
+    }
+    return items
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <FlatList 
-        data = {[{key: 'Ariadni'}]}
-        renderItem = {({item}) => <Text> {item.key} </Text>} 
+        <FlatList style={{ flex: 1 }}
+        keyExtractor = { item => item.id }
+        data = {this.getTasks()}
+        renderItem = {({item}) => <TaskCell task={item}> </TaskCell>}
         />
       </View>
     );
   }
 }
 
+class TaskCell extends Component<Props> {
+  render() {
+    return (
+      <View style={{flexDirection: 'row', padding: 2, borderBottomWidth: StyleSheet.hairlineWidth }}>
+          <View style={{ justifyContent: 'center', alignItems: 'center'}}>
+              <CheckBox style= {{ marginLeft: 10, marginRight: 10 }} />
+          </View>
+          <View style={{height: 50, flex: 1, alignItems: 'stretch', flexDirection: 'column', justifyContent: 'space-between'}}>
+            <View style={{ flex: 1}}>
+              <Text>
+                {this.props.task.title}
+              </Text>      
+            </View>
+            <View style={{ flex: 1}}>
+            <Text>
+                {this.props.task.date}
+                {this.props.task.hour}
+            </Text>
+            </View>
+          </View>
+      </View>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
+  taskCellItem: {
+    height: 40,
+  },
+  taskCell: {
+    flex: 1,
+    flexDirection: 'column',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
     backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
+  }
 });
